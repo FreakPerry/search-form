b00f4eb63b15baa5a81e105087111cb6699fe1dd
FreakPerry <elisei.tatarenko@gmail.com>
2024-03-03 13:48:54 +0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { FavouritesPages } from './pages/FavouritesPages';
+import { Navigation } from './components/Navigation';
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favourites" element={<FavouritesPages />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
